923a6c58af6d0f9ff5b36894a5a2f1714594383d
Xnoroms <52173778+Xnoroms@users.noreply.github.com>
2020-05-12 08:45:32 +0200

    abstract class

---
+++ src/main/java/human.java
@@ -0,0 +1,12 @@
+public abstract class human extends mammals{
+    public human(String n, String o, int m, int l) {
+        super(n, o, m, l);
+    }
+
+    public human() {
+
+    }
+
+    public abstract void thinkAbstractly();
+
+}
